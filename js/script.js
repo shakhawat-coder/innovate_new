d53da318c0ec4ada6dc0752f50ba6d8e23617413
shakhawat <shakhawathossen188@gmail.com>
2023-08-26 21:57:31 +0600

    project setup

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$('.counter').counterUp({
+    delay: 10,
+    time: 1000
+});
+
+
+$('.slider_wrapper').slick({
+    infinite: true,
+    prevArrow:".prev",
+    nextArrow:".next"
+  });
+
+
+  AOS.init();
